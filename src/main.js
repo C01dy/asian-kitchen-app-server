f0e8ccc8264cd94289ceb827560079b2baedebd0
vlad.m <vlad.moskvichev@progforce.com>
2023-09-28 17:24:13 +0300

    fix/feat: fix module aliases, add Dockerfile for build node app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 'use strict';
-const server = require('./http/http.js')
 const fsp = require('fs').promises;
 const path = require('path');
+const moduleAlias = require('module-alias')
+
+moduleAlias.addAliases({
+  '@db': path.resolve(__dirname, 'db', 'db.js'),
+  '@util': path.resolve(__dirname, 'util'),
+  '@http': path.resolve(__dirname, 'http', 'http.js'),
+  '@error': path.resolve(__dirname, 'error')
+})
+
+moduleAlias()
+
 const load = require('./load.js');
+const server = require('@http')
 const db = require('@db');
 const util = require('@util')
 
@@ -18,7 +29,7 @@ const routing = {
     if (!fileName.endsWith('.js')) continue;
     const filePath = path.join(apiPath, fileName);
     const serviceName = path.basename(fileName, '.js');
-    routing[serviceName] = await load(filePath, sandbox);
+    routing[`api/${serviceName}`] = await load(filePath, sandbox);
   }
 
   // staticServer('./static', 8000);
